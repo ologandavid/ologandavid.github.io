dea62fccbe2beaf2f016d9aff68f14036e7111e1
ologandavid <66838084+ologandavid@users.noreply.github.com>
2026-02-10 23:48:28 +0000

    Deploying to gh-pages from @ ologandavid/ologandavid.github.io@22fc8f9b1c28a2124174c5d419dc99d4a70c1b4d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -117,7 +117,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/David_Ologan_Resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
