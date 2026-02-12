289a53ec1f438e33afcc56aea1372717117f17fc
ologandavid <66838084+ologandavid@users.noreply.github.com>
2026-02-12 02:47:15 +0000

    Deploying to gh-pages from @ ologandavid/ologandavid.github.io@a4472721b3918005c85a05cd6d6ebecd08b652e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,18 +64,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-multi-robot-motion-planning-for-quadruped-robots",
+            },},{id: "projects-multi-robot-motion-planning-for-quadruped-robots",
           title: 'Multi-Robot Motion Planning for Quadruped Robots',
           description: "16.782 Planning and Decision Making in Robotics (Fall 2023)",
           section: "Projects",handler: () => {
