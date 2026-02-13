91dbddb489108b28f048ef13b478274f0a85d7a6
ologandavid <66838084+ologandavid@users.noreply.github.com>
2026-02-13 23:49:35 +0000

    Deploying to gh-pages from @ ologandavid/ologandavid.github.io@945a87c79e380d4a1b269222e22a9ec8ea071506 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Teaching and Outreach Activities",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
